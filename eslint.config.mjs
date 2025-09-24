22d0790bf1cd757b7d3a8449b813cfe228ceaa1e
Marcos Curbeco <marcoscurbeco03@gmail.com>
2025-09-24 08:46:06 -0600

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+    plugins: { prettier },
+    rules: {
+      ...js.configs.recommended.rules,
+      "prettier/prettier": "error"
+    },
+  },
+  pluginReact.configs.flat.recommended,
+]);
