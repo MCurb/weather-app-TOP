4e5e4ddc8e5c5c9d0cae92fb8e94815915892362
MCurb <marcoscurbeco03@gmail.com>
2025-09-25 13:08:41 -0600

    Refactor and display weather data

--- src/display-weather-data.js
+++ src/display-weather-data.js
@@ -1,20 +1,34 @@
 import { getWeatherData } from './get-weather-data';
-
 const inputSearch = document.querySelector('.input-search');
 let locationSearch = 'Holguin Cuba';
 
-const temp = document.querySelector('.temp');
-const feels = document.querySelector('.feels-like');
-const icn = document.querySelector('.icon');
+// Weather Overview
 const locationAddress = document.querySelector('.location');
+const temp = document.querySelector('.current-temp');
+const feels = document.querySelector('.feels-like');
+const condition = document.querySelector('.condition-status');
+
+// Weather Details
+const wind = document.querySelector('.wind-data');
+const uv = document.querySelector('.uv-index-data');
+const humidityData = document.querySelector('.humidity-data');
+const rain = document.querySelector('.rain-chance-data');
+const sunriseTime = document.querySelector('.sunrise-data');
+const sunsetTime = document.querySelector('.sunset-data');
 
 export async function displayData() {
-  const { temperature, icon, feelsLike, location } = await takeTheDataINeed();
+  const { paths } = await takeTheDataINeed();
 
-  //   temp.textContent = temperature;
-  //   feels.textContent = feelsLike;
-  //   icn.textContent = icon;
-  //   locationAddress.textContent = location;
+  locationAddress.textContent = paths.location;
+  temp.textContent = `${paths.temperature}°F`;
+  feels.textContent = `Feels like: ${paths.feelsLike}°F`;
+  condition.textContent = paths.conditionStatus;
+  wind.textContent = `${paths.windSpeed} mph`;
+  uv.textContent = paths.uvIndex;
+  humidityData.textContent = `${paths.humidity}%`;
+  rain.textContent = `${paths.rainChance}%`;
+  sunriseTime.textContent = paths.sunrise;
+  sunsetTime.textContent = paths.sunset;
 }
 
 export function handleFormData(e) {
@@ -25,10 +39,20 @@ export function handleFormData(e) {
 
 async function takeTheDataINeed() {
   const jsonData = await getWeatherData(locationSearch);
-
-  const temperature = jsonData.currentConditions.temp;
-  const icon = jsonData.currentConditions.icon;
-  const feelsLike = jsonData.days[0].feelslike;
-  const location = jsonData.resolvedAddress;
-  return { temperature, icon, feelsLike, location };
+  const currentConditions = jsonData.currentConditions;
+  const paths = {
+    location: jsonData.resolvedAddress,
+    temperature: currentConditions.temp,
+    feelsLike: currentConditions.feelslike,
+    conditionStatus: currentConditions.conditions,
+    windSpeed: currentConditions.windspeed,
+    uvIndex: currentConditions.uvindex,
+    humidity: currentConditions.humidity,
+    rainChance: currentConditions.precipprob,
+    sunrise: currentConditions.sunrise,
+    sunset: currentConditions.sunset,
+  };
+  return {
+    paths,
+  };
 }
